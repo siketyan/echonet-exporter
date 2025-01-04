24febe055f2071962845c45db986bafcabf1e35c
Naoki Ikeguchi <me@s6n.jp>
2025-01-05 02:53:41 +0900

    refactor: Move server to the dedicated file

--- src/main.zig
+++ src/main.zig
@@ -17,6 +17,7 @@ const BP35C0 = transport.BP35C0(SerialPort);
 
 const TransactionManager = @import("./transaction.zig").TransactionManager;
 const Controller = @import("./controller.zig").Controller;
+const Server = @import("./server.zig").Server(Controller(BP35C0));
 
 // const packet = @import("./packet.zig");
 // const Ip6Packet = packet.Ip6Packet;
@@ -25,7 +26,7 @@ const Controller = @import("./controller.zig").Controller;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    // defer std.debug.assert(gpa.deinit() == .ok);
+    defer std.debug.assert(gpa.deinit() == .ok);
 
     const conf = try config.Config.loadYamlFileAlloc("config.yaml", allocator);
     defer conf.deinit();
@@ -57,110 +58,9 @@ pub fn main() !void {
     };
 
     const addr = try net.Address.parseIp4("0.0.0.0", 9100);
-    var server = try addr.listen(.{ .reuse_address = true });
-    log.info("HTTP server is ready at {}", .{std.fmt.Formatter(net.Address.format){ .data = addr }});
+    var server = Server.init(allocator, addr, conf, &txm, &controller);
 
-    var rx_buf: [2048]u8 = undefined;
-    var tx_buf: [2048]u8 = undefined;
-
-    while (true) {
-        const conn = try server.accept();
-        defer conn.stream.close();
-
-        log.info("A new connection from {} has been accepted", .{
-            std.fmt.Formatter(net.Address.format){ .data = conn.address },
-        });
-
-        var http_server = http.Server.init(conn, &rx_buf);
-        while (http_server.state == .ready) {
-            var request = http_server.receiveHead() catch continue;
-
-            log.info("{s} {s} {s}", .{
-                @tagName(request.head.version),
-                @tagName(request.head.method),
-                request.head.target,
-            });
-
-            if (!mem.eql(u8, request.head.target, "/metrics")) {
-                try request.respond(&.{}, .{ .status = .not_found });
-                log.info("404 Not Found", .{});
-                continue;
-            }
-
-            if (request.head.method != .GET) {
-                try request.respond(&.{}, .{ .status = .method_not_allowed });
-                log.info("405 Method Not Allowed", .{});
-                continue;
-            }
-
-            var props = try echonet.PropertyList.init(allocator, conf.measures.items.len);
-            defer props.deinit();
-            for (conf.measures.items) |measure| {
-                try props.list.append(.{ .epc = measure.epc, .edt = null });
-            }
-
-            const tid = txm.begin();
-            const req = echonet.Frame{
-                .format1 = .{
-                    .tid = tid,
-                    .edata = .{
-                        .seoj = .{
-                            .class_group_code = 0x05,
-                            .class_code = 0xFF,
-                            .instance_code = 0x01,
-                        },
-                        .deoj = .{
-                            .class_group_code = conf.target.class_group_code,
-                            .class_code = conf.target.class_code,
-                            .instance_code = conf.target.instance_code,
-                        },
-                        .esv = 0x62, // Get
-                        .props = props,
-                    },
-                },
-            };
-
-            const resp = try controller.handle(req) orelse {
-                // TODO: Retry
-                try request.respond(&.{}, .{ .status = .gateway_timeout });
-                log.info("504 Gateway Timeout", .{});
-                continue;
-            };
-            defer resp.deinit();
-
-            var response = request.respondStreaming(.{
-                .send_buffer = &tx_buf,
-                .respond_options = .{
-                    .extra_headers = &.{
-                        .{ .name = "Content-Type", .value = "text/plain; version=0.0.4" },
-                    },
-                },
-            });
-            defer response.end() catch {};
-            defer log.info("200 OK", .{});
-
-            for (resp.format1.edata.props.asSlice()) |prop| {
-                const measure: config.Measure = for (conf.measures.items) |m| {
-                    if (m.epc == prop.epc) {
-                        break m;
-                    }
-                } else continue;
-
-                const edt = prop.edt orelse unreachable;
-                const value = switch (measure.type) {
-                    .signed_long => mem.readInt(i32, edt.items[0..4], .big),
-                };
-
-                const name = measure.name.asSlice();
-                if (measure.help) |help| {
-                    try std.fmt.format(response.writer(), "# HELP {s} {s}\n", .{ name, help.asSlice() });
-                }
-
-                try std.fmt.format(response.writer(), "# TYPE {s} gauge\n", .{name});
-                try std.fmt.format(response.writer(), "{s} {d}\n", .{ name, value });
-            }
-        }
-    }
+    try server.run();
 }
 
 comptime {
---
+++ src/server.zig
@@ -0,0 +1,157 @@
+const std = @import("std");
+const http = std.http;
+const log = std.log.scoped(.server);
+const mem = std.mem;
+const net = std.net;
+
+const config = @import("./config.zig");
+const echonet = @import("./echonet.zig");
+const TransactionManager = @import("./transaction.zig").TransactionManager;
+
+fn fmtAddress(addr: net.Address) std.fmt.Formatter(net.Address.format) {
+    return std.fmt.Formatter(net.Address.format){ .data = addr };
+}
+
+pub fn Server(comptime Controller: type) type {
+    return struct {
+        const Self = @This();
+
+        allocator: mem.Allocator,
+        addr: net.Address,
+        conf: config.Config,
+        txm: *TransactionManager,
+        controller: *const Controller,
+        rx_buf: [2048]u8 = undefined,
+        tx_buf: [2048]u8 = undefined,
+
+        pub fn init(
+            allocator: mem.Allocator,
+            addr: net.Address,
+            conf: config.Config,
+            txm: *TransactionManager,
+            controller: *const Controller,
+        ) Self {
+            return Self{
+                .allocator = allocator,
+                .addr = addr,
+                .conf = conf,
+                .txm = txm,
+                .controller = controller,
+            };
+        }
+
+        pub fn run(self: *Self) !void {
+            var server = try self.addr.listen(.{
+                .reuse_address = true,
+            });
+
+            log.info("HTTP server is ready at {}", .{fmtAddress(self.addr)});
+
+            while (true) {
+                const conn = try server.accept();
+                defer conn.stream.close();
+
+                log.info("A new connection from {} has been accepted", .{fmtAddress(conn.address)});
+
+                try self.handleConnection(conn);
+            }
+        }
+
+        fn handleConnection(self: *Self, conn: net.Server.Connection) !void {
+            var http_server = http.Server.init(conn, &self.rx_buf);
+            while (true) {
+                var request = http_server.receiveHead() catch continue;
+
+                log.info("{s} {s} {s}", .{
+                    @tagName(request.head.version),
+                    @tagName(request.head.method),
+                    request.head.target,
+                });
+
+                try self.handleRequest(&request);
+            }
+        }
+
+        fn handleRequest(self: *Self, request: *http.Server.Request) !void {
+            if (!mem.eql(u8, request.head.target, "/metrics")) {
+                try request.respond(&.{}, .{ .status = .not_found });
+                log.info("404 Not Found", .{});
+                return;
+            }
+
+            if (request.head.method != .GET) {
+                try request.respond(&.{}, .{ .status = .method_not_allowed });
+                log.info("405 Method Not Allowed", .{});
+                return;
+            }
+
+            var props = try echonet.PropertyList.init(self.allocator, self.conf.measures.items.len);
+            defer props.deinit();
+            for (self.conf.measures.items) |measure| {
+                try props.list.append(.{ .epc = measure.epc, .edt = null });
+            }
+
+            const tid = self.txm.begin();
+            const target = self.conf.target;
+            const req = echonet.Frame{
+                .format1 = .{
+                    .tid = tid,
+                    .edata = .{
+                        .seoj = .{
+                            .class_group_code = 0x05,
+                            .class_code = 0xFF,
+                            .instance_code = 0x01,
+                        },
+                        .deoj = .{
+                            .class_group_code = target.class_group_code,
+                            .class_code = target.class_code,
+                            .instance_code = target.instance_code,
+                        },
+                        .esv = 0x62, // Get
+                        .props = props,
+                    },
+                },
+            };
+
+            const resp = try self.controller.handle(req) orelse {
+                // TODO: Retry
+                try request.respond(&.{}, .{ .status = .gateway_timeout });
+                log.info("504 Gateway Timeout", .{});
+                return;
+            };
+            defer resp.deinit();
+
+            var response = request.respondStreaming(.{
+                .send_buffer = &self.tx_buf,
+                .respond_options = .{
+                    .extra_headers = &.{
+                        .{ .name = "Content-Type", .value = "text/plain; version=0.0.4" },
+                    },
+                },
+            });
+            defer response.end() catch {};
+            defer log.info("200 OK", .{});
+
+            for (resp.format1.edata.props.asSlice()) |prop| {
+                const measure: config.Measure = for (self.conf.measures.items) |m| {
+                    if (m.epc == prop.epc) {
+                        break m;
+                    }
+                } else continue;
+
+                const edt = prop.edt orelse unreachable;
+                const value = switch (measure.type) {
+                    .signed_long => mem.readInt(i32, edt.items[0..4], .big),
+                };
+
+                const name = measure.name.asSlice();
+                if (measure.help) |help| {
+                    try std.fmt.format(response.writer(), "# HELP {s} {s}\n", .{ name, help.asSlice() });
+                }
+
+                try std.fmt.format(response.writer(), "# TYPE {s} gauge\n", .{name});
+                try std.fmt.format(response.writer(), "{s} {d}\n", .{ name, value });
+            }
+        }
+    };
+}
